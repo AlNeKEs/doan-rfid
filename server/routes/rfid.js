1d2fb4cce64f4fcdf9e56e5a515130ec57157381
AlNeKEs <linhle05101999@gmail.com>
2022-11-13 17:48:46 +0700

    server

---
+++ server/routes/rfid.js
@@ -0,0 +1,85 @@
+var mqtt = require("mqtt");
+const express = require("express");
+const app = express();
+const router = express.Router();
+const client = mqtt.connect("mqtt://broker.mqttdashboard.com");
+const Device = require("../models/Device");
+
+const http = require("http");
+const { Server } = require("socket.io");
+const rfidServer = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+  client.on("connect", function () {
+    console.log("Connected");
+  });
+  client.on("error", function (error) {
+    console.log(error);
+  });
+  io.on("connection", (socket) => {
+    console.log(`User connected ${socket.id}`);
+    socket.on("disconnect", () => {
+      console.log(`user disconnect `);
+    });
+    socket.on("fromclient", function (data) {
+      console.log(data);
+      if (data.message == "add") {
+        client.publish(
+          "doan/rfid/subcribetopic",
+          JSON.stringify({ message: "add" })
+        );
+      } else if (data.message == "scan") {
+        client.publish(
+          "doan/rfid/subcribetopic",
+          JSON.stringify({ message: "scan" })
+        );
+      }
+    });
+
+    // -- read RFID card for add and update
+    client.subscribe("doan/rfid/publishtopic/add");
+    client.on("message", (topic, message) => {
+      // called each time a message is received
+      const cardID = message.toString().trim();
+      socket.emit("addFromServer", {
+        success: true,
+        message: "Successfully",
+        rfidId: cardID,
+      });
+    });
+
+    // scan RFID card
+    client.subscribe("doan/rfid/publishtopic/scan");
+    client.on("message", async (topic, message) => {
+      // called each time a message is received
+      const cardID = message.toString().trim();
+      const data = await Device.find({ rfidId: cardID });
+      try {
+        if (data[0] != null) {
+          socket.emit("scanFromServer", {
+            success: true,
+            message: "Successfully",
+            data: data[0],
+          });
+        } else {
+          const mess = {
+            message: "not found",
+            rfidId: cardID,
+          };
+          socket.emit("scanFromServer", {
+            success: false,
+            message: "Successfully",
+            data: cardID,
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  });
+};
+module.exports = rfidServer;
